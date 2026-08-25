e95f997052c606291661733b0f7a3f2f80efe77c
Fiza Zahid <fizazahid.136824@marwadiuniversity.ac.in>
2026-08-25 11:26:25 -0700

    Update U2_P8_1.java

--- unit2/U2_P8_1.java
+++ unit2/U2_P8_1.java
@@ -5,12 +5,12 @@
 
     public static void main(String[] args)
      {
-     final int Diya_roll_no=126;
+     final int Fiza_roll_no=149;
      final double max_marks = 100.0;
 
-     System.out.println("Diya's roll no:" + Diya_roll_no);
+     System.out.println("Fiza's roll no:" + Fiza_roll_no);
 
-     //Diya_roll_no=126;   
+     //Fiza_roll_no=149;   
 
      }
  }
